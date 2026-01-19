3a15bd939b408de6bc6c5abe1a34f0699a4730b7
shivanshu <shivanshumangal007@gmail.com>
2026-01-19 23:44:52 +0530

    added form handler

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const formhandler = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+
+    console.log("email : ", email )
+    console.log("password : ", password )
+  };
   return (
     <div className="loginMainDiv">
       <div className="loginDiv">
         <h1>login</h1>
         <div className="inputs">
-          <h2>email:</h2>
-          <input type="email" placeholder="enter your email" />
-          <h2>password:</h2>
-          <input type="password" placeholder="enter your password" />
-
-          <input type="submit" />
+          <form
+            action=""
+            onSubmit={(e) => {
+              formhandler(e);
+            }}
+          >
+            <h2>email:</h2>
+            <input
+              type="email"
+              placeholder="enter your email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              value={email}
+            />
+            <h2>password:</h2>
+            <input
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              type="password"
+              placeholder="enter your password"
+            />
+            <input type="submit" />
+          </form>
         </div>
       </div>
     </div>
