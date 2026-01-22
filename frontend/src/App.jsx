2f03a7eb03d567a6a4e0d60f8fdb243d1ec2be40
Hannu Verma <hannuverma78@gmail.com>
2026-01-22 22:59:16 +0530

    fix app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div>
       <Loader />
-      <Login /> */}
+      <Login /> 
       {/* <EmplyoeeDashboard/>
         <Routes>
           <Route path='/' element={<MainPage/>}/>
