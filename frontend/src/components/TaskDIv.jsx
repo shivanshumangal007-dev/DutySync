6e3e9c4e03cfe19984fb20ca817d4ef4a96b6c00
shivanshu <shivanshumangal007@gmail.com>
2026-01-23 22:52:26 +0530

    logout frontend

--- frontend/src/components/TaskDIv.jsx
+++ frontend/src/components/TaskDIv.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import axios from "axios";
-
+import {useNavigate} from "react-router-dom";
 const TaskDiv = ({ tasks }) => {
   // const [task, setTasks] = useState([]);
   // const [loading, setLoading] = useState(true);
@@ -22,40 +22,50 @@ const TaskDiv = ({ tasks }) => {
   // }, []);
 
   // if (loading) return <p>Loading tasks...</p>;
-
+  const navigate = useNavigate();
   const completeHandler = async (taskId) => {
     try {
-        const response = await axios.patch(
-            `http://127.0.0.1:8000/task/${taskId}/update/`,
-            { status: "COMPLETED" }, // Data being sent
-            { withCredentials: true } // Keeps you logged in
-        );
+      const response = await axios.patch(
+        `http://127.0.0.1:8000/task/${taskId}/update/`,
+        { status: "COMPLETED" }, // Data being sent
+        { withCredentials: true }, // Keeps you logged in
+      );
 
-        console.log(response.data.message);
+      console.log(response.data.message);
 
-        // Refresh the page or update the local state so the UI changes
-        window.location.reload(); 
+      // Refresh the page or update the local state so the UI changes
+      window.location.reload();
     } catch (error) {
-        console.error("Failed to update task", error);
+      console.error("Failed to update task", error);
     }
-  }
+  };
+
 
   return (
     <div className="taskdiv">
-      {tasks.map((task, key) => (
-        <div className="taskbox" key={key}>
-          <div className="uppermost">
-            <span className= {`task-flag ${task.priority === "HIGH" ? 'red-flag' : task.priority == "MEDIUM" ? 'orange-flag' : 'red-flag'}`}>{task.priority}</span>
-            <span className="due-date">Due date: {task.due_date}</span>
-          </div>
-          <h1>{task.title}</h1>
-          <p>{task.description}</p>
-          <div className="btn">
-            <button onClick={() => completeHandler(task.id)}>mark as complete</button>
-          </div>
-        </div>
-      ))}
-      {/* <TaskDiv/> */}
+      {tasks.map((task, key) => {
+        if (task.status !== "COMPLETED") {
+          return (
+            <div className="taskbox" key={key}>
+              <div className="uppermost">
+                <span
+                  className={`task-flag ${task.priority === "HIGH" ? "red-flag" : task.priority == "MEDIUM" ? "orange-flag" : "red-flag"}`}
+                >
+                  {task.priority}
+                </span>
+                <span className="due-date">Due date: {task.due_date}</span>
+              </div>
+              <h1>{task.title}</h1>
+              <p>{task.description}</p>
+              <div className="btn">
+                <button onClick={() => completeHandler(task.id)}>
+                  mark as complete
+                </button>
+              </div>
+            </div>
+          );
+        }
+      })}
       {/* <div className="taskbox">
         <div className="uppermost">
           <span className="task-flag">high</span>
@@ -67,6 +77,33 @@ const TaskDiv = ({ tasks }) => {
           <button>mark as complete</button>
         </div>
       </div> */}
+      {tasks.map((task, key) => {
+        if (task.status === "COMPLETED") {
+          return (
+            <div className="taskbox completed" key={key}>
+              <div className="uppermost">
+                <span
+                  className={`task-flag ${task.priority === "HIGH" ? "red-flag" : task.priority == "MEDIUM" ? "orange-flag" : "red-flag"}`}
+                >
+                  {task.priority}
+                </span>
+                <span className="due-date">Due date: {task.due_date}</span>
+              </div>
+              <h1>{task.title}</h1>
+              <p>{task.description}</p>
+              <div className="btn">
+                <button
+                  style={{ backgroundColor: "green" }}
+                  disabled
+                  onClick={() => completeHandler(task.id)}
+                >
+                  completed
+                </button>
+              </div>
+            </div>
+          );
+        }
+      })}
     </div>
   );
 };
